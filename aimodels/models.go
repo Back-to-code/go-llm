7e94a093d1fa45314c4ae181b0beab26528d96db
mjarkk <mkopenga@gmail.com>
2026-08-06 09:45:25 +0200

    update ai models

--- aimodels/models.go
+++ aimodels/models.go
@@ -22,26 +22,26 @@ func GetModel(name string) *llm.Model {
 var (
 	// The best models with with the option to think.
 	// Should be used if the mini model is not good enough.
-	// By deafult the models use the lowest option of thinking (so no thinking in most cases), the level of thinking can be enabled inside llm.Options
-	// = PRICY - ULTRA EXPENSIVE
-	ChatGpt5   = register("gpt-5.4", &openai.Provider{})
+	// By default the models use the lowest option of thinking (so no thinking in most cases), the level of thinking can be enabled inside llm.Options
+	// = PRICY - ULTRA TURBO EXPENSIVE
+	ChatGpt5   = register("gpt-5.6-sol", &openai.Provider{})
 	Gemini3Pro = register("gemini-3.1-pro-preview", &googleaistudio.Provider{})
-	Best       = ChatGpt5 // <- Deafult
+	Best       = ChatGpt5 // <- Default
 
 	// Mini models.
 	// When the nano model is not good enough but the good model is somewhat too expensive
 	// This is most of the time a good middleground
-	// = CHEAP
-	ChatGpt5Mini = register("gpt-5.4-mini", &openai.Provider{})
-	Gemini3Flash = register("gemini-3-flash-preview", &googleaistudio.Provider{})
-	Mini         = ChatGpt5Mini // <- Deafult
+	// = OKE ISH PRICE
+	ChatGpt5Mini     = register("gpt-5.4-mini", &openai.Provider{})
+	Gemini3FlashLite = register("gemini-3.5-flash-lite", &googleaistudio.Provider{})
+	Mini             = ChatGpt5Mini // <- Default
 
 	// Default nano model.
 	// For basic llm tasks mainly smart parttern matching tasks are these models perfect for
 	// Or giving simple things a score.
 	// = DIRT CHEAP
-	ChatGpt5Nano = register("gpt-5-nano", &openai.Provider{})
-	Gemini2Flash = register("gemini-2.5-flash-lite", &googleaistudio.Provider{})
+	ChatGpt5Nano = register("gpt-5-nano", &openai.Provider{})                    // Note that 5.4 nano and 5.6 luna are much more expensive
+	Gemini2Flash = register("gemini-2.5-flash-lite", &googleaistudio.Provider{}) // Note that 3.5 flash lite is much more expensive than 2.5 flash lite
 	Mercury2     = register("mercury-2", &inception.Provider{})
-	Nano         = ChatGpt5Nano // <- Deafult
+	Nano         = ChatGpt5Nano // <- Default
 )
